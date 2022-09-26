93d1fb28a79492c3f28dce8861df4567ee01946a
krishnamohanparuchuri <paruchurikris@gmail.com>
2022-09-26 11:28:28 +0200

    add cookie to the frontend and get cookie rsponse

--- controllers/authControllers.go
+++ controllers/authControllers.go
@@ -1,11 +1,17 @@
 package controllers
 
 import (
+	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"github.com/krishna/jwt_go/database"
 	"github.com/krishna/jwt_go/models"
 	"golang.org/x/crypto/bcrypt"
 )
+
+const SecretKey = "this is a very very secret"
 
 func Register(c *fiber.Ctx) error {
 
@@ -51,5 +57,29 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	return c.JSON(user)
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.Id)),
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(SecretKey))
+
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError)
+		return c.JSON(fiber.Map{
+			"message": "Could not login",
+		})
+	}
+
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
